878b82f2caf853b75cf5ee00bbed0f246174fa91
sirjla <joseluis@assadi.io>
2018-12-26 18:02:55 -0300

    fix(labels): fix bug where labels were not being filtered correctly

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{ board.name }}</h1>
     <h2>Tag Filter</h2>
-    <v-select multiple v-model="selectedLabelOptions" v-bind:options="cardLabels" />
+    <v-select multiple v-model="selectedCardLabels" v-bind:options="selectedLabelOptions" />
     <h2>Board Status</h2>
     <BoardInfo
         v-bind:lists="lists"
@@ -70,8 +70,10 @@ export default {
         '5bcf863f74837934564848c8',
       ],
       lists: [],
+      allCardsByList: {},
       cardsByList: {},
       listIncludesArchived: ['5bcf863f74837934564848c8'],
+      allCardsActivities: [],
       cardActivities: [],
       fillBackLists: true,
       dateTypeSelector: 'day',
@@ -83,56 +85,70 @@ export default {
   mounted() {
     this.getLists(this.$props.board.id, this.listIds);
     this.getBoardLabels(this.$props.board.id);
+    this.getSelectedCards();
   },
   watch: {
-    selectedLabelOptions() {
-      this.selectedCardLabels = this.selectedLabelOptions.map((option) => option.value);
-      this.lists.forEach((list) => this.getCards(list.id, this.listIncludesArchived.includes(list.id)));
+    selectedCardLabels() {
+      this.getSelectedCards();
+      this.getSelectedActivities();
     },
   },
   methods: {
+    getSelectedCards() {
+      this.lists.forEach((list) => {
+        this.cardsByList[list.id] = this.allCardsByList[list.id].filter(
+          (card) => !card.labels.map((label) => label.id).some((cardLabel) => this.selectedCardLabels.map((selectedLabel) => selectedLabel.value).includes(cardLabel)));
+      });
+    },
+    getSelectedActivities() {
+      this.cardActivities = this.allCardsActivities.filter(
+        (activity) => Object.values(this.cardsByList).flat().map((card) => card.id)
+          .includes(activity.data.card.id)
+      );
+    },
     getLists(boardId, listIds) {
       const self = this;
       request(
         `boards/${boardId}/lists`,
         (response) => {
           self.lists = response.data.filter((element) => listIds.includes(element.id));
-          self.lists.forEach((element) => self.getCards(element.id, self.listIncludesArchived.includes(element.id)));
+          self.lists.forEach((element) => self.getAllCards(element.id, self.listIncludesArchived.includes(element.id)));
         },
         () => {
           onRequestError(self.getLists, [boardId, listIds]);
         }
       );
     },
-    getCards(listId, includeArchived) {
+    getAllCards(listId, includeArchived) {
       const cardsFilter = includeArchived ? 'all' : 'open';
       const self = this;
-      self.$set(self.cardsByList, listId, []);
+      self.$set(self.allCardsByList, listId, []);
+      self.$set(this.cardsByList, listId, []);
       request(
         `lists/${listId}`,
         (response) => {
-          const filteredCards = [];
           response.data.cards.forEach((card) => {
-            if (!card.labels.map((label) => label.id).some((cardLabel) => self.selectedCardLabels.includes(cardLabel))) {
-              filteredCards.push(card);
-              self.getCardActivities(card.id);
-            }
+            self.getAllCardsActivities(card.id);
           });
-          self.cardsByList[listId] = filteredCards;
+          self.allCardsByList[listId] = response.data.cards;
+          self.cardsByList[listId] = response.data.cards;
         },
         () => {
-          onRequestError(self.getCards, [listId, includeArchived]);
+          onRequestError(self.getAllCards, [listId, includeArchived]);
         },
         { cards: cardsFilter }
       );
     },
-    getCardActivities(cardId) {
+    getAllCardsActivities(cardId) {
       const self = this;
       request(
         `cards/${cardId}/actions`,
-        (response) => { self.cardActivities = self.cardActivities.concat(response.data); },
+        (response) => {
+          self.allCardsActivities = self.allCardsActivities.concat(response.data);
+          self.cardActivities = self.cardActivities.concat(response.data);
+        },
         () => {
-          onRequestError(self.getCardActivities, [cardId]);
+          onRequestError(self.getAllCardsActivities, [cardId]);
         },
         { filter: 'createCard,updateCard:idList' }
       );
@@ -142,7 +158,7 @@ export default {
       request(
         `boards/${boardId}/labels`,
         (response) => {
-          self.cardLabels = response.data.map((label) => ({ label: label.name, value: label.id }));
+          self.selectedLabelOptions = response.data.map((label) => ({ label: label.name, value: label.id }));
         },
         () => {
           onRequestError(self.getBoardLabels, [boardId]);
