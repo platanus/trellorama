52ff82d0161f7755d231c2209608f4d5c7c93fde
sirjla <joseluis@assadi.io>
2018-12-26 12:29:31 -0300

    feat(proyection chart): create proyection chart component

---
+++ src/components/ProyectionChart.vue
@@ -0,0 +1,46 @@
+<script>
+import { Line, mixins } from 'vue-chartjs';
+
+const { reactiveData } = mixins;
+
+export default {
+  name: 'ProyectionChart',
+  mixins: [Line, reactiveData],
+  props: {
+    cardActivities: {
+      type: Array,
+      default: null,
+    },
+    speed: Number,
+    timeUnitsForward: Number,
+  },
+  data() {
+    return {
+      chartoptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        chartdata: {},
+      },
+    };
+  },
+  watch: {
+    cardActivities() {
+      this.renderData();
+    },
+    speed() {
+      this.renderData();
+    },
+    timeUnitsForward() {
+      this.renderData();
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.chartoptions);
+  },
+  methods: {
+    renderData() {
+      this.renderChart(this.chartdata, this.chartoptions);
+    },
+  },
+};
+</script>
