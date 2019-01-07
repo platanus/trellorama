1d8d65c0deb0ee5ae5a3ffb8e41083a513fe5127
sirjla <joseluis@assadi.io>
2019-01-07 12:09:20 -0300

    feat(projectionDate): implement projection date

--- src/components/ProjectionWrapper.vue
+++ src/components/ProjectionWrapper.vue
@@ -20,20 +20,25 @@
         <option value="sunday">Sunday</option>
       </select>
       &ensp;
-      <label for="timeUnits">Time units to project: </label>
-      <input type="number" id="timeUnits" v-model="timeUnits">
-      &ensp;
       <label for="optimistValue">Optimist increase: </label>
       <input type="number" id="optimistValue" v-model="optimistValue">
       &ensp;
       <label for="pesimistValue">Pesimist decrease: </label>
       <input type="number" id="timeUpesimistValuenits" v-model="pesimistValue">
+      &ensp;
+      <div class="date-selector">
+        <div class="date-selector--input">
+          <label for="endDate">Projection Date: </label>
+          <datepicker v-model="projectionDate" name="projectionDate" placeholder="Projection Date" format="yyyy-MM-dd"/>
+        </div>
+      </div>
+      {{a}}
     </div>
     <ProjectionChart
       v-bind:filteredActivities="filteredActivities"
       v-bind:endListId="endListId"
       v-bind:speed="parseFloat(localSpeedProjection(filteredActivities))"
-      v-bind:timeUnitsForward="parseInt(timeUnits)"
+      v-bind:timeUnitsForward="timeUnits"
       v-bind:dateTypeSelector="dateTypeSelector"
       v-bind:optimistValue="parseInt(optimistValue)"
       v-bind:pesimistValue="parseInt(pesimistValue)"
@@ -45,11 +50,13 @@
 </template>
 
 <script>
+import Datepicker from 'vuejs-datepicker';
+import moment from 'moment';
 import ProjectionChart from './ProjectionChart.vue';
 import { filterActivities, speedProjection } from '../utils/speedUtil.js';
 import { get, save } from '../utils/configurationPersistance.js';
 
-const baseProjectionTimeUnits = 5;
+moment().format('yyyy-MM-dd');
 
 export default {
   name: 'ProjectionWrapper',
@@ -63,16 +70,22 @@ export default {
   },
   components: {
     ProjectionChart,
+    Datepicker,
   },
   data() {
     return {
       dateTypeSelector: get(`${this.boardId}_projection_dateType`, 'week'),
       dayOfWeek: get(`${this.boardId}_projection_dayOfWeek`, 'monday'),
-      timeUnits: get(`${this.boardId}_projection_timeUnits`, baseProjectionTimeUnits),
       filteredActivities: [],
       optimistValue: get(`${this.boardId}_projection_optimistValue`, 1),
       pesimistValue: get(`${this.boardId}_projection_pesimistValue`, 1),
+      projectionDate: get(`${this.boardId}_projection_projectionDate`, new Date()),
     };
+  },
+  computed: {
+    timeUnits() {
+      return Math.ceil(moment(this.projectionDate).diff(moment(new Date()), `${this.dateTypeSelector}s`, true));
+    },
   },
   mounted() {
     this.generateData();
@@ -89,8 +102,8 @@ export default {
       save(`${this.boardId}_projection_dateType`, this.dateTypeSelector);
       this.generateData();
     },
-    timeUnits() {
-      save(`${this.boardId}_projection_timeUnits`, this.timeUnits);
+    projectionDate() {
+      save(`${this.boardId}_projection_projectionDate`, this.projectionDate);
     },
     optimistValue() {
       save(`${this.boardId}_projection_optimistValue`, this.optimistValue);
@@ -115,3 +128,15 @@ export default {
 };
 
 </script>
+
+<style>
+.date-selector {
+  display: flex;
+  justify-content: center;
+}
+.date-selector--input {
+  margin: 10px;
+  display: flex;
+}
+</style>
+
