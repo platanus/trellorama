6ff7aa42443a15c9fd5f72d526ebe41bdf17e9f6
sirjla <joseluis@assadi.io>
2019-01-08 12:46:18 -0300

    feat(WIPLists): create view for WIP lists

---
+++ src/components/WIPList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>{{ list.name }}</h3>
+    <div class="container">
+      <Card v-for="card in sortedCards" :key="card.id" v-bind:card="card"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+import Card from './Card.vue';
+
+export default {
+  name: 'WIPList',
+  props: {
+    list: Object,
+    cards: Array,
+  },
+  components: {
+    Card,
+  },
+  computed: {
+    sortedCards() {
+      return this.cards.slice().sort((a, b) => moment(a.dateLastActivity) - moment(b.dateLastActivity));
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+}
+</style>
---
+++ src/components/WIPLists.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Work In Progress Lists</h2>
+    <WIPList
+      v-for="list in lists"
+      v-bind:key="list.id"
+      v-bind:list="list"
+      v-bind:cards="cards[list.id]"
+    />
+  </div>
+</template>
+
+<script>
+import WIPList from './WIPList.vue';
+
+export default {
+  name: 'WIPLists',
+  props: {
+    lists: Array,
+    cards: Object,
+  },
+  components: {
+    WIPList,
+  },
+};
+</script>
