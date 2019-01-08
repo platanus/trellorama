30d8873ac6aa670a616ec4aedd3bf15c99ebabb7
sirjla <joseluis@assadi.io>
2019-01-08 17:54:31 -0300

    fix(line length): fix line length warning

--- src/utils/speedUtil.js
+++ src/utils/speedUtil.js
@@ -18,7 +18,9 @@ function speedProjection(filteredActivities, startDate, endDate) {
   const finalDate = (endDate === null) ? moment() : moment(endDate);
   const initialDate = (startDate === null) ? filteredActivities[0].date : startDate;
 
-  return (filteredActivities.length / Math.ceil(filteredActivities.length === 0 ? 1 : finalDate.diff(initialDate, 'weeks', true)))
+  return (filteredActivities.length / Math.ceil(
+    filteredActivities.length === 0 ? 1 : finalDate.diff(initialDate, 'weeks', true)
+  ))
     .toFixed(1);
 }
 
