58d5a26f017eb0c371afbdba5306d051672200eb
sirjla <joseluis@assadi.io>
2019-01-11 18:33:22 -0300

    feat(sentry): implement sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import dotenv from 'dotenv';
 import App from './App.vue';
+import * as Sentry from '@sentry/browser'
 
 dotenv.config();
 
@@ -9,3 +10,8 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
+Sentry.init({
+  dsn: 'https://279544681984435792b43230421d4610@sentry.io/1369885',
+  integrations: [new Sentry.Integrations.Vue({ Vue })]
+})
