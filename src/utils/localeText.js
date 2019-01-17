ec9a3a9209eeef0c8c901daa5d04a85fd2739389
sirjla <joseluis@assadi.io>
2019-01-17 11:44:48 -0300

    feat(locale): initialize locale

---
+++ src/utils/localeText.js
@@ -0,0 +1,14 @@
+const messages = {
+  en: {
+    dashboard: {
+      labelFilter: 'Label Filter',
+    },
+  },
+  es: {
+    dashboard: {
+      labelFilter: 'Filtro de Etiquetas',
+    },
+  },
+};
+
+export default messages;
