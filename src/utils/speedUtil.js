6835383379e3347db17d217264a0d3ae5e6d272e
sirjla <joseluis@assadi.io>
2019-01-17 15:13:45 -0300

    fix(projection): fix projection chart bug

--- src/utils/speedUtil.js
+++ src/utils/speedUtil.js
@@ -7,10 +7,25 @@ function sortDate(activity1, activity2) {
   return moment(activity1.date).diff(activity2.date, 'days');
 }
 
+function filterActivitiesByMovedTwiceInSameList(activity, _, array) {
+  return array.filter((filterActivity) => filterActivity.id === activity.id)
+    .sort((activity1, activity2) => (activity1.date - activity2.date))[0]
+    .id === activity.id;
+}
+
+function filterDuplicates(activity, _, array) {
+  if (array.filter((filterActivity) => filterActivity.id === activity.id).length === 1) {
+    return true;
+  }
+
+  return filterActivitiesByMovedTwiceInSameList(activity, _, array);
+}
+
 function filterActivities(activities, endListIds, dateTypeSelector, dayOfWeek = 'monday') {
   return activities.filter((activity) => activity.type === 'updateCard')
     .filter((activity) => endListIds.includes(activity.data.listAfter.id))
     .map((activity) => ({ id: activity.data.card.id, date: getDate(activity.date, dateTypeSelector, dayOfWeek) }))
+    .filter((activity, _, array) => filterDuplicates(activity, _, array))
     .sort(sortDate);
 }
 
