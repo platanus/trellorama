157bb9f020f22bc43683bd01ceca899343bdc1d5
sirjla <joseluis@assadi.io>
2019-01-23 15:25:58 -0300

    style(dashboard): add transition on dashboard

--- src/components/PastDashboard.vue
+++ src/components/PastDashboard.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <CumulativeWrapper
-      v-if="tab === 'cumulative'"
-      v-bind:cardActivities="cardActivities"
-      v-bind:listIds="listIds"
-      v-bind:boardId="boardId"
-      v-bind:startDate="startDate"
-      v-bind:endDate="endDate"
-    />
-    <WipHistogramWrapper
-      v-if="tab === 'histogram'"
-      v-bind:wipLists="wipLists"
-      v-bind:cardActivities="cardActivities"
-    />
+    <transition name="toggle" mode="out-in">
+      <CumulativeWrapper
+        v-if="tab === 'cumulative'"
+        v-bind:cardActivities="cardActivities"
+        v-bind:listIds="listIds"
+        v-bind:boardId="boardId"
+        v-bind:startDate="startDate"
+        v-bind:endDate="endDate"
+      />
+      <WipHistogramWrapper
+        v-if="tab === 'histogram'"
+        v-bind:wipLists="wipLists"
+        v-bind:cardActivities="cardActivities"
+      />
+    </transition>
   </div>
 </template>
 
