083750e6d69b0e9508e7a4f1114f5ec510a173fa
sirjla <joseluis@assadi.io>
2019-01-23 10:28:27 -0300

    feat(dasboard options): add dashboard button component

---
+++ src/components/DashboardOption.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="dashboard-options--option">
+  <button :class="buttonClass" v-on:click="buttonFunction">
+    <font-awesome-icon :icon="icon" />
+  </button>
+  <p class="dashboard-options--text" v-if="!minimized">{{ text }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'dashboardOption',
+  props: {
+    text: String,
+    minimized: Boolean,
+    icon: String,
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+    buttonFunction: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  computed: {
+    buttonClass() {
+      return {
+        'button': true,
+        'dashboard-options--button': true,
+        'dashboard-options--button-active': this.selected,
+      };
+    },
+  },
+};
+</script>
