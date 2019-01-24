3cf38e4ec9cb5683ba4206a13428f086d7887cf2
sirjla <joseluis@assadi.io>
2019-01-24 11:01:11 -0300

    feat(wip histogram): implement sqrt bin size

--- src/components/wipHistogram.vue
+++ src/components/wipHistogram.vue
@@ -3,7 +3,9 @@ import { Bar } from 'vue-chartjs';
 import { getTimes } from '../utils/timeBetweenLists.js';
 import { getColor } from '../utils/chartUtils.js';
 
-const decimalRoundParameter = 10;
+const decimalRoundParameter = 100;
+const numberOfDecimals = 2;
+const decimalBase = 10;
 
 export default {
   name: 'wipHistogram',
@@ -17,11 +19,19 @@ export default {
         responsive: true,
         maintainAspectRatio: false,
         scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
+          xAxes: [
+            {
+              barPercentage: 1.0,
+              categoryPercentage: 1.0,
             },
-          }],
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
         },
       },
       chartdata: {
@@ -33,13 +43,25 @@ export default {
   computed: {
     listTimes() {
       return getTimes(...this.activities)
-        .map((time) => Math.round(time * decimalRoundParameter) / decimalRoundParameter);
+        .map((time) => Math.round(time * decimalRoundParameter) / decimalRoundParameter)
+        .sort((a, b) => a - b);
+    },
+    binSize() {
+      return Math.ceil(Math.sqrt(this.listTimes.length));
     },
     labels() {
-      return Array.from(new Set(this.listTimes)).sort((a, b) => a - b);
+      const width = (this.listTimes[this.listTimes.length - 1] - this.listTimes[0]) / this.binSize;
+
+      return [...Array(this.binSize).keys()].map((label) => label * width);
     },
     data() {
-      return this.labels.map((label) => this.listTimes.filter((time) => time === label).length);
+      const data = [...Array(this.labels.length).keys()];
+
+      return data.map((_, index) =>
+        this.listTimes.filter((time) => time >= this.labels[index])
+          .filter((time) => (index === this.labels.length - 1 ? true : time < this.labels[index + 1]))
+          .length
+      );
     },
   },
   watch: {
@@ -52,14 +74,22 @@ export default {
   },
   methods: {
     renderData() {
-      this.chartdata.labels = this.labels;
+      this.chartdata.labels = this.genLabels();
       this.chartdata.datasets[0] = {
         label: '',
-        labels: this.labels,
+        labels: this.genLabels(),
         data: this.data,
         backgroundColor: getColor('blue')[0],
+        borderColor: getColor('red')[0],
       };
       this.renderChart(this.chartdata, this.chartoptions);
+    },
+    genLabels() {
+      return this.labels.map((label, index, arr) =>
+        `[${label.toFixed(numberOfDecimals, decimalBase)} - ${arr[index + 1] === undefined ?
+          this.listTimes[this.listTimes.length - 1].toFixed(numberOfDecimals, decimalBase) :
+          arr[index + 1].toFixed(numberOfDecimals, decimalBase)}[`
+      );
     },
   },
 };
