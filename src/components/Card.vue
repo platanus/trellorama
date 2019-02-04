226802467690b0c602edcaac27be8c44de3b3229
sirjla <joseluis@assadi.io>
2019-02-04 20:07:34 -0300

    feat(cards): implement styles for cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,23 +1,26 @@
 <template>
-  <div :class="classObject" :style="style" v-on:click="goToTrello()">
+  <div :class="classObject" v-on:click="goToTrello()">
     <div class="card-label__container">
       <div v-for="label in cardLabels" :key="label.id"
         class="card-label"
         :style="{ backgroundColor: getTrelloLabelColor(label.color) }">
-          <p>{{ label.name }}</p>
       </div>
     </div>
-    <h4 class="card-name">{{ card.name }}</h4>
-    <p><b>{{ $t('wip.days') }}: </b>{{ days }}</p>
-    <div class="card-member__container">
-      <img v-for="member in cardMembers" :key="member.id"
-        class="member-avatar" :src="getImage(member)">
+    <p class="card-name">{{ card.name }}</p>
+    <div class="card-info__container">
+      <p :class="bellClass"><font-awesome-icon icon="bell" /> {{ days }} {{ $t('wip.days') }}</p>
+      <div class="card-member__container">
+        <img v-for="member in cardMembers" :key="member.id"
+          class="member-avatar" :src="getImage(member)">
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 /* global require */
+
+const two = 2;
 
 export default {
   name: 'Card',
@@ -36,13 +39,13 @@ export default {
   },
   computed: {
     classObject() {
+      const days = parseFloat(this.days);
+
       return {
         card: true,
-      };
-    },
-    style() {
-      return {
-        backgroundColor: this.genBackgroundColor(),
+        'card-ok': days < this.average,
+        'card-bad': days - (this.average * two) > 0,
+        'card-warning': days - this.average > 0 && days - this.average < this.average,
       };
     },
     cardLabels() {
@@ -51,14 +54,15 @@ export default {
     cardMembers() {
       return this.allMembers.filter((member) => this.card.idMembers.includes(member.id));
     },
+    bellClass() {
+      const days = parseFloat(this.days);
+
+      return {
+        'bell-class': days - (this.average * two) > 0,
+      };
+    },
   },
   methods: {
-    genBackgroundColor() {
-      const days = parseFloat(this.days);
-      if (days < this.average) return '#eaeaea';
-
-      return `rgba(255, 0, 0, ${(days - this.average) / this.average})`;
-    },
     goToTrello() {
       window.open(this.card.shortUrl, '_blank');
     },
